15395c99c035aeb08c8e3773cfffb676252df465
Aayush Singh <aayushsingh1239@gmail.com>
2025-05-24 14:50:23 +0545

    Add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className='p-10'>
-      <Button>Click me</Button>
+    <div >
+      
     </div>
   )
 }
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Subscription = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            subs
+        </div>
+    )
 }
 
 export default Subscription
