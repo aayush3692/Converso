d8eebf21f60ae9c1c8953b6e68e635b1190c24af
Aayush Singh <aayushsingh1239@gmail.com>
2025-06-12 08:40:04 +0545

    new files added

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,10 +1,40 @@
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companions.actions';
+import { getSubjectColor } from '@/lib/utils';
 import React from 'react'
 
-const CompanionLibrary = () => {
-    return (
-        <div>
+const CompanionLibrary =async  ({searchParams}: SearchParams) => {
+    const filter = await searchParams;
+    const subject = filter.subject ? filter.subject : '';
+    const topic = filter.topic? filter.topic: '';
+
+    const companions = await getAllCompanions({ subject, topic});
+
+    console.log(companions)
+    return (    
+        <main>
+            <section  className='flex justify-between gap-4 max-sm:flex-col'>
+                <h1>Companion Library</h1>
+                <div className='flex gap-4'>
+                    <SearchInput/>
+                    <SubjectFilter/>
+                </div>
+                </section>
+                
+                <section className='companion-grid'>
+                    {companions.map((companion) => (
+                        <CompanionCard key={companion.id} {...companion} 
+                        color={getSubjectColor(companion.subject)}
+                        />
+                    ))}
+
+                </section>
+
             
-        </div>
+
+        </main>
     )
 }
 
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export const createSupabaseClient = () => {
     return createClient(
-        process.env.NEXT_Public_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
             async accessToken() {
                 return ((await auth()).getToken())
